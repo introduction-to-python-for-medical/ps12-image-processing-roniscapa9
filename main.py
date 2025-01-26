3fee684e59f8fa1ef95ab07c7af02f8bd8929b4e
roniscapa9 <roniscapa9@gmail.com>
2025-01-26 15:05:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+def suppress_noise(image_array):
+  clean_image = median(image_array, ball(3))
+  return clean_image
+def detect_edges (clean_image):
+  edges = edge_detection(clean_image)
+  return edges
+def convert_to_binary (edges, threshold):
+  binary_image = (edges > threshold).astype(np.uint8)
+  return binary_image
+def save_binary_image (binary_image, file_name):
+  edge_image = Image.fromarray(binary_image * 255)
+  edge_image.save(file_name)
 
+image_array = load_image('roni.jpg')
+clean_image = suppress_noise(image_array)
+edges = detect_edges(clean_image)
+binary_edges = convert_to_binary(edges, threshold=50)
+save_binary_image(binary_edges, 'my_edges.png')
