8b13b384efa8eae1df69ecf76b9bdc908c14e2eb
roniscapa9 <roniscapa9@gmail.com>
2025-01-26 15:21:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ import matplotlib.pyplot as plt
 def suppress_noise(image_array):
   clean_image = median(image_array, ball(3))
   return clean_image
+
 def detect_edges (clean_image):
   edges = edge_detection(clean_image)
   return edges
+
 def convert_to_binary (edges, threshold):
   binary_image = (edges > threshold).astype(np.uint8)
   return binary_image
+
 def save_binary_image (binary_image, file_name):
   edge_image = Image.fromarray(binary_image * 255)
   edge_image.save(file_name)
@@ -24,3 +27,7 @@ clean_image = suppress_noise(image_array)
 edges = detect_edges(clean_image)
 binary_edges = convert_to_binary(edges, threshold=50)
 save_binary_image(binary_edges, 'my_edges.png')
+
+plt.imshow(binary_edges, cmap='gray')
+plt.title("binary edges")
+plt.show()
